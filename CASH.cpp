fd404ce5a4e664ea97f95718f4f0d0d81bfffce1
apooorvasri <38426238+apooorvasri@users.noreply.github.com>
2020-09-25 01:53:34 +0530

    Update CASH.cpp

--- CASH.cpp
+++ CASH.cpp
@@ -21,7 +21,7 @@ int main() {
 	    long long int a[n+1],b[n+1];
 	    a[n]=0;
 	    b[n]=0;
-	    
+	    //for loop
 	    for(int i=0;i<n;i++){
 	        cin>>a[i];}
 	        for(int i=0;i<n;i++){
